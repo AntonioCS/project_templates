cfedd6e2e3f3ef7a0ce5d4d08f88bbbb6f76ea2a
AntonioCS <antoniocs@gmail.com>
2024-05-30 11:22:23 +0100

    Symfony project updates

--- php/basic/src/app.php
+++ php/basic/src/app.php
@@ -1,5 +1,16 @@
 <?php
 declare(strict_types=1);
 
+use Root\App\bla1;
+
 require 'vendor/autoload.php';
 
+
+spl_autoload_register(static function(string $class): void {
+    echo $class , "\n";
+    if ($class === 'Root\App\bla2') {
+        require 'src/mocks/bla2.php';
+    }
+},true,true);
+
+new bla1();
