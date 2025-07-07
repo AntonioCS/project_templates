eb7ae2a90bda81161c1250a823310948c7eeab70
AntonioCS <antoniocs@gmail.com>
2025-07-07 08:22:59 +0100

    Improvements

--- php/symfony/bind-hub/mb_config.mk
+++ php/symfony/bind-hub/mb_config.mk
@@ -2,15 +2,9 @@
 mb_project_name := symfony-php8
 mb_project_prefix := sy
 
-### List of modules to include in the project
-### This can be MakeBind modules or custom modules located in the modules folder in <your_project>/BindHub/modules
-mb_project_modules := docker/docker_compose.mk \
-	docker/php/composer.mk \
-	docker/php/symfony.mk \
-	docker/nginx.mk
 
 dc_files := $(mb_project_path)/docker/docker-compose.yml \
 	$(mb_project_path)/docker/docker-compose.dev.yml
 
-dc_service_nginx := app_nginx
-dc_service_php := app
+nginx_dc_service := app_nginx
+php_dc_service := app
